784722dee8a6457fbc7256393f376a8506aee9b0
Payal Sikarwar <sonasikarwar16@gmail.com>
2026-08-02 21:19:28 +0530

    Create AddTwoIntegers.java

---
+++ AddTwoIntegers.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class integer
+{
+    public static void main (String args [])
+     {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter first number");
+        int num1 = in.nextInt();
+
+        System.out.println("Enter second number");
+        int num2 = in.nextInt();
+        int sum = num1+num2;
+        System.out.println("Sum:"+sum);
+
+        input.close();
+      }
+}
